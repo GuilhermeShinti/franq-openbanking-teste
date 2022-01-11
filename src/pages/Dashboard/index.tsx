becfc79d9853399626e6f60768396b04dd88b970
GuilhermeShinti <guilhermeshinti@gmail.com>
2022-01-11 17:12:28 -0300

    implement dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,17 +1,33 @@
 import { useEffect, useState } from "react"
 import { api } from "../../services/api";
 
+interface IIndexes {
+    IBOVESPA: IIndex
+    IFIX: IIndex
+    NASDAQ: IIndex
+    DOWJONES: IIndex
+    CAC: IIndex,
+    NIKKEI: IIndex
+}
+
+interface IIndex {
+    name: string,
+    location: string,
+    points: number,
+    variation: number,
+}
+
 export function Dashboard() {
-    const [currencies, setCurrencies] = useState<[]>([]);
-    const [stocks, setStocks] = useState<[]>([]);
-    const [bitcoin, setBitcoin] = useState<[]>([]);
+    const [currencies, setCurrencies] = useState({});
+    const [stocks, setStocks] = useState<IIndex[]>([]);
+    const [bitcoin, setBitcoin] = useState({});
 
     const getFinances = async () => {
         await api.get('finance').then(
             response => {
                 const {bitcoin, currencies, stocks} = response.data.results
                 setCurrencies(currencies);
-                setStocks(stocks);
+                setStocks(Object.values(stocks[0]));
                 setBitcoin(bitcoin);
             }
         )
@@ -24,9 +40,11 @@ export function Dashboard() {
     return (
         <>
             {
-                Object.keys(stocks).map(stock => 
+                stocks.map(index => 
                     (
-                        <div>{stock}</div>
+                        <div key={index.name}>
+                            {index.name}
+                        </div>
                     )
                 )
             }
